6e1d070b36caf4dadc8b54db81f427db0eca7be1
VadimChorrny <chorrny228@gmail.com>
2021-04-22 14:43:53 +0300

    Добавьте файлы проекта.

---
+++ Paint_Demo/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace Paint_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnDarkWhite = new System.Windows.Forms.Button();
+            this.btnWhiteBlue = new System.Windows.Forms.Button();
+            this.btnDarkRed = new System.Windows.Forms.Button();
+            this.btnMediaAqua = new System.Windows.Forms.Button();
+            this.btnWhiteRed = new System.Windows.Forms.Button();
+            this.btnAqua = new System.Windows.Forms.Button();
+            this.btnSilver = new System.Windows.Forms.Button();
+            this.btnFuchsia = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnSizePlus = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(154, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(634, 426);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.Location = new System.Drawing.Point(12, 12);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(34, 34);
+            this.btnBlue.TabIndex = 0;
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnDarkWhite
+            // 
+            this.btnDarkWhite.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnDarkWhite.Location = new System.Drawing.Point(92, 132);
+            this.btnDarkWhite.Name = "btnDarkWhite";
+            this.btnDarkWhite.Size = new System.Drawing.Size(34, 34);
+            this.btnDarkWhite.TabIndex = 1;
+            this.btnDarkWhite.UseVisualStyleBackColor = false;
+            this.btnDarkWhite.Click += new System.EventHandler(this.btnDarkWhite_Click);
+            // 
+            // btnWhiteBlue
+            // 
+            this.btnWhiteBlue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnWhiteBlue.Location = new System.Drawing.Point(52, 132);
+            this.btnWhiteBlue.Name = "btnWhiteBlue";
+            this.btnWhiteBlue.Size = new System.Drawing.Size(34, 34);
+            this.btnWhiteBlue.TabIndex = 2;
+            this.btnWhiteBlue.UseVisualStyleBackColor = false;
+            this.btnWhiteBlue.Click += new System.EventHandler(this.btnWhiteBlue_Click);
+            // 
+            // btnDarkRed
+            // 
+            this.btnDarkRed.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnDarkRed.Location = new System.Drawing.Point(12, 132);
+            this.btnDarkRed.Name = "btnDarkRed";
+            this.btnDarkRed.Size = new System.Drawing.Size(34, 34);
+            this.btnDarkRed.TabIndex = 3;
+            this.btnDarkRed.UseVisualStyleBackColor = false;
+            this.btnDarkRed.Click += new System.EventHandler(this.btnDarkRed_Click);
+            // 
+            // btnMediaAqua
+            // 
+            this.btnMediaAqua.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.btnMediaAqua.Location = new System.Drawing.Point(92, 92);
+            this.btnMediaAqua.Name = "btnMediaAqua";
+            this.btnMediaAqua.Size = new System.Drawing.Size(34, 34);
+            this.btnMediaAqua.TabIndex = 4;
+            this.btnMediaAqua.UseVisualStyleBackColor = false;
+            this.btnMediaAqua.Click += new System.EventHandler(this.btnMediaAqua_Click);
+            // 
+            // btnWhiteRed
+            // 
+            this.btnWhiteRed.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnWhiteRed.Location = new System.Drawing.Point(52, 92);
+            this.btnWhiteRed.Name = "btnWhiteRed";
+            this.btnWhiteRed.Size = new System.Drawing.Size(34, 34);
+            this.btnWhiteRed.TabIndex = 5;
+            this.btnWhiteRed.UseVisualStyleBackColor = false;
+            this.btnWhiteRed.Click += new System.EventHandler(this.btnWhiteRed_Click);
+            // 
+            // btnAqua
+            // 
+            this.btnAqua.BackColor = System.Drawing.Color.Aqua;
+            this.btnAqua.Location = new System.Drawing.Point(12, 92);
+            this.btnAqua.Name = "btnAqua";
+            this.btnAqua.Size = new System.Drawing.Size(34, 34);
+            this.btnAqua.TabIndex = 6;
+            this.btnAqua.UseVisualStyleBackColor = false;
+            this.btnAqua.Click += new System.EventHandler(this.btnAqua_Click);
+            // 
+            // btnSilver
+            // 
+            this.btnSilver.BackColor = System.Drawing.Color.Silver;
+            this.btnSilver.Location = new System.Drawing.Point(92, 52);
+            this.btnSilver.Name = "btnSilver";
+            this.btnSilver.Size = new System.Drawing.Size(34, 34);
+            this.btnSilver.TabIndex = 7;
+            this.btnSilver.UseVisualStyleBackColor = false;
+            this.btnSilver.Click += new System.EventHandler(this.btnSilver_Click);
+            // 
+            // btnFuchsia
+            // 
+            this.btnFuchsia.BackColor = System.Drawing.Color.Fuchsia;
+            this.btnFuchsia.Location = new System.Drawing.Point(52, 52);
+            this.btnFuchsia.Name = "btnFuchsia";
+            this.btnFuchsia.Size = new System.Drawing.Size(34, 34);
+            this.btnFuchsia.TabIndex = 8;
+            this.btnFuchsia.UseVisualStyleBackColor = false;
+            this.btnFuchsia.Click += new System.EventHandler(this.btnFuchsia_Click);
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.Location = new System.Drawing.Point(92, 12);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(34, 34);
+            this.btnRed.TabIndex = 9;
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.BackColor = System.Drawing.Color.Green;
+            this.btnGreen.Location = new System.Drawing.Point(12, 52);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(34, 34);
+            this.btnGreen.TabIndex = 10;
+            this.btnGreen.UseVisualStyleBackColor = false;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.btnYellow.Location = new System.Drawing.Point(52, 12);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(34, 34);
+            this.btnYellow.TabIndex = 11;
+            this.btnYellow.UseVisualStyleBackColor = false;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // btnSizePlus
+            // 
+            this.btnSizePlus.Location = new System.Drawing.Point(12, 172);
+            this.btnSizePlus.Name = "btnSizePlus";
+            this.btnSizePlus.Size = new System.Drawing.Size(114, 42);
+            this.btnSizePlus.TabIndex = 0;
+            this.btnSizePlus.Text = "+";
+            this.btnSizePlus.UseVisualStyleBackColor = true;
+            this.btnSizePlus.Click += new System.EventHandler(this.btnSizePlus_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 42);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 156);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CLEAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSizePlus);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.btnFuchsia);
+            this.Controls.Add(this.btnSilver);
+            this.Controls.Add(this.btnAqua);
+            this.Controls.Add(this.btnWhiteRed);
+            this.Controls.Add(this.btnMediaAqua);
+            this.Controls.Add(this.btnDarkRed);
+            this.Controls.Add(this.btnWhiteBlue);
+            this.Controls.Add(this.btnDarkWhite);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Paint | Chorrny Inc";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnDarkWhite;
+        private System.Windows.Forms.Button btnWhiteBlue;
+        private System.Windows.Forms.Button btnDarkRed;
+        private System.Windows.Forms.Button btnMediaAqua;
+        private System.Windows.Forms.Button btnWhiteRed;
+        private System.Windows.Forms.Button btnAqua;
+        private System.Windows.Forms.Button btnSilver;
+        private System.Windows.Forms.Button btnFuchsia;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnSizePlus;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Paint_Demo/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint_Demo
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        #region 
+        private Color color = Color.Black; // default color
+        private int size = 5;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Colors Set Buttons
+        /// </summary>
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            color = Color.Blue;
+        }
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            color = Color.Yellow;
+        }
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            color = Color.Red;
+        }
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            color = Color.Green;
+        }
+        private void btnFuchsia_Click(object sender, EventArgs e)
+        {
+            color = Color.Fuchsia;
+        }
+        private void btnSilver_Click(object sender, EventArgs e)
+        {
+            color = Color.Silver;
+        }
+        private void btnAqua_Click(object sender, EventArgs e)
+        {
+            color = Color.Aqua;
+        }
+        private void btnWhiteRed_Click(object sender, EventArgs e)
+        {
+            color = Color.PaleVioletRed;
+        }
+        private void btnMediaAqua_Click(object sender, EventArgs e)
+        {
+            color = Color.MediumAquamarine;
+        }
+        private void btnDarkRed_Click(object sender, EventArgs e)
+        {
+            color = Color.DarkRed;
+        }
+        private void btnWhiteBlue_Click(object sender, EventArgs e)
+        {
+            color = Color.BlueViolet;
+        }
+        private void btnDarkWhite_Click(object sender, EventArgs e)
+        {
+            color = Color.WhiteSmoke;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Label lbl = new Label();
+                lbl.BackColor = color;
+                lbl.Size = new Size(size,size);
+                lbl.Location = e.Location;
+                lbl.BringToFront();
+                this.panel1.Controls.Add(lbl);
+            }
+        }
+
+        private void btnSizePlus_Click(object sender, EventArgs e)
+        {
+            if (size <= 100)
+                size++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (size > 0)
+                size--;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.panel1.Controls.Clear();
+        }
+    }
+}
